1b2586f07774d13e8ddc9bd84c6a349edba8d403
dmcruz <dmcruz@gmail.com>
2017-04-19 23:31:33 +0800

    Delete Post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { getPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+  
   render() {
-    return <div>Show post {this.props.params.id}</div>;
+    const { post } = this.props;
+    if (!post) {
+      return <div>Loading</div>;
+    }
+
+    return (
+      <div>
+        <Link to="/">Back to index</Link>
+        <button className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+
+      </div>
+    );
+  }
+
+  componentWillMount() {
+    this.props.getPost(this.props.params.id);
+
+  }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id)
+      .then(() => {
+        this.context.router.push('/');
+      });
   }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+export default connect(mapStateToProps, { getPost, deletePost })(PostsShow);
